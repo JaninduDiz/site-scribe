597e3f238b9e2b981888274a77a7521bd65f1b1b
Janindu Dissanayake <janindu001@gmail.com>
2025-08-30 09:07:16 +0000

    i want to add a custom icon for this app, for favicon and all other plac

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#D4A373',
     icons: [
       {
-        src: "https://placehold.co/192x192.png",
+        src: "/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "https://placehold.co/512x512.png",
+        src: "/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
